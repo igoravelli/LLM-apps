bd87361d22794a3ecd5addbcd2ab051c14cb3659
Igor Ravelli <igorregly@gmail.com>
2023-12-08 22:08:28 -0300

    split document into chunks

--- section-05/packages/functions.py
+++ section-05/packages/functions.py
@@ -25,3 +25,17 @@ def load_from_wikipedia(query, lang='en', load_max_docs=2):
     data = loader.load()
 
     return data
+
+def chunk_documents(data, chunk_size=256):
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=0)
+    chunks = text_splitter.split_documents(data)
+    
+    return chunks
+
+def print_embedding_cost(texts):
+    import tiktoken
+    enc = tiktoken.encoding_for_model('text-embedding-ada-002')
+    total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
+    print(f'Total Tokens: {total_tokens}')
+    print(f'Embedding Cost in USD: {total_tokens / 1000 * 0.0004:.6f}')
