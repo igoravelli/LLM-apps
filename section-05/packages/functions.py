a532131fecda23eae1e1edfa6f88f863c6a4d319
Igor Ravelli <igorregly@gmail.com>
2023-12-08 22:24:26 -0300

    inserting new indexes into pinecone

--- section-05/packages/functions.py
+++ section-05/packages/functions.py
@@ -39,3 +39,45 @@ def print_embedding_cost(texts):
     total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
     print(f'Total Tokens: {total_tokens}')
     print(f'Embedding Cost in USD: {total_tokens / 1000 * 0.0004:.6f}')
+
+def insert_or_fetch_embeddings(index_name):
+    import pinecone
+    from langchain.vectorstores import Pinecone
+    from langchain.embeddings.openai import OpenAIEmbeddings
+
+    embeddings = OpenAIEmbeddings()
+    pinecone.init(
+        api_key=os.environ.get('PINECONE_API_KEY'),
+        environment=os.environ.get('PINECONE_ENV')
+    )
+
+    if index_name in pinecone.list_indexes():
+        print(f'Index {index_name} already exists. Loading embeddings...')
+        vector_store = Pinecone.from_existing_index(index_name, embeddings)
+        print('OK')
+    else:
+        print(f'Creating index {index_name} and embeddings..')
+        pinecone.create_index(index_name, dimension=1536, metric='cosine')
+        vector_store = Pinecone.from_documents(chunks, embeddings, index_name=index_name)
+        print('OK')
+
+    return vector_store
+
+def delete_pinecone_index(index_name='all'):
+    import pinecone
+    pinecone.init(
+        api_key=os.environ.get('PINECONE_API_KEY'),
+        environment=os.environ.get('PINECONE_ENV')
+    )
+
+    if index_name == 'all':
+        indexes = pinecone.list_indexes()
+        print('Deleting all indexes...')
+        for index in indexes:
+            pinecone.delete_index(index)
+        print('OK')
+    else:
+        print(f'Deleting index {index_name}')
+        pinecone.delete_index(index_name)
+        print('OK')
+
