3f79f5708afa67a2b42e7e0b927402bcdfa5d6ad
igorravelli <igorregly@gmail.com>
2023-12-10 23:44:37 -0300

    organizing files and folders

--- section-07/packages/functions.py
+++ section-07/packages/functions.py
@@ -12,3 +12,9 @@ def instantiate_llm(model_name="gpt-3.5-turbo"):
     
     return llm
 
+def print_embedding_cost(texts):
+    import tiktoken
+    enc = tiktoken.encoding_for_model('text-embedding-ada-002')
+    total_tokens = sum([len(enc.encode(page.page_content)) for page in texts])
+    print(f'Total Tokens: {total_tokens}')
+    print(f'Embedding Cost in USD: {total_tokens / 1000 * 0.0004:.6f}')
